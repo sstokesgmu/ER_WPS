62271fc5f362041a2190db64dd5e43d48c6e7400
Steam Deck User <deck@steamdeck.mynetworksettings.com>
2025-04-14 23:46:30 -0400

    Working on completing the socket server, I created a simple skeleton for how I want the script to flow. Now we need to make a error handling function, possible a try catch, and create template data to send over UDP

--- Server/src/main.lua
+++ Server/src/main.lua
@@ -1,13 +1,45 @@
 print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-local socket = require("socket")
+local socket = require "socket"
 
-local server = assert(socket.bind("*", 12345))
-local ip, port = server:getsockname()
+Server = { protocol = nil, ip = nil, portNumber = nil }
 
-print("Server running on " .. ip .. ':' .. port)
+function Server.EstablishSocket(socket, address, port)
+    local udp = socket.udp()
+    --Server Settings
+    udp:settimeout(0);
+    udp:setsockname(address, port)
+    local ip, port = udp:getsockname()
+    return udp, ip, port
+end
+
+local server = Server.EstablishSocket(socket, '*', 10180)
+--Setup
+print("Server is created,  ip: " .. server.ip .. " and the port number: " .. server.portNumber);
 
 
 
+-- local running = true
 
+-- --Check for clients
+-- local clients = {}
+
+-- --Main Server Loop
+-- print("Beginning server loop")
+-- while running do
+--     -- Listen for request
+--     local data, ip, port = udp:receivefrom()
+
+--     --New data
+
+
+--     --If data is received
+--     if data then
+--         print("Data recieved from ip: " .. ip)
+--         --Format the data then send the to the client map server
+--     else
+--         print("No data is recieved")
+--         socket.sleep(1);
+--     end
+-- end
 print
 ('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
