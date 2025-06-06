01fc3f6e2d02efb8112c777aa139b8d76f8f94ef
sstokesgmu <sterlingjstokes@gmail.com>
2025-06-06 00:18:46 -0400

    Completed first pass at KD tree, it works well with simple test definately would need to refactor

--- NodeServer/kdTree.js
+++ NodeServer/kdTree.js
@@ -1,7 +1,6 @@
-
 class Node {
   constructor(point, depth) {
-    (this.point = point), this.left, this.right, this.splittingAxis = depth;
+    (this.point = point), this.left, this.right, (this.splittingAxis = depth);
   }
   //? When I start to assign left and right do I need to do a type check
 }
@@ -52,7 +51,7 @@ class KDTree {
     //? How do know if the current node is the leaf : ANSWER they have no children nodes (left and right = null)
 
     if (currentNode.left == null && currentNode.right == null) {
-      console.log("Found Leaf Node ... Begining Back Tracking ...")
+      console.log("Found Leaf Node ... Begining Back Tracking ...");
       return {
         closestPoint: currentNode,
         minDistance: this.DistanceTo(target, currentNode.point),
@@ -60,65 +59,40 @@ class KDTree {
     }
 
     const splittingAxis = depth % this.k;
-    // splittingAxis == 0 ? console.log(`Comparing based on the X Axis ${splittingAxis}`) : console.log(`Comparing based on the Y Axis ${splittingAxis}`) 
+    // splittingAxis == 0 ? console.log(`Comparing based on the X Axis ${splittingAxis}`) : console.log(`Comparing based on the Y Axis ${splittingAxis}`)
     if (
       target[splittingAxis] >= currentNode.point[splittingAxis] &&
       currentNode.right != null
     ) {
-
-
-      obj = this.NearestToTarget(
-        target,
-        currentNode.right,
-        depth + 1,
-      );
+      obj = this.NearestToTarget(target, currentNode.right, depth + 1);
     } else if (currentNode.left != null) {
-      obj = this.NearestToTarget(
-        target,
-        currentNode.left,
-        depth + 1,
-      );
+      obj = this.NearestToTarget(target, currentNode.left, depth + 1);
     }
 
     //* Peek if the axis is different from splitting axis of the node
 
-    
     // if(obj.closestPoint.splittingAxis != currentNode.splittingAxis){
     //   console.log("There is a possible better point on the other side of this axis: " + currentNode.point);
     //   console.log("This calls spliting axis was:  " + splittingAxis)
     //   console.log("This is the current best guess' splitting axis: " + obj.closestPoint.splittingAxis)
     // }
-    
+
     //* Is the parent node actually closer than the original min distance
     if (this.DistanceTo(target, currentNode.point) < obj.minDistance) {
-
-        obj = {
-          closestPoint: currentNode,
-          minDistance: this.DistanceTo(target, currentNode.point),
-        }
-    }
-    else if(obj.closestPoint.splittingAxis != currentNode.splittingAxis)
-    {
-       //* Do a search on the next axis
-            if (
-          target[splittingAxis] >= currentNode.point[splittingAxis] &&
-          currentNode.right != null
-        ) {
-
-
-          obj = this.NearestToTarget(
-            target,
-            currentNode.right,
-            depth + 1,
-          );
-        } else if (currentNode.left != null) {
-          obj = this.NearestToTarget(
-            target,
-            currentNode.left,
-            depth + 1,
-          );
-        }
-
+      obj = {
+        closestPoint: currentNode,
+        minDistance: this.DistanceTo(target, currentNode.point),
+      };
+    } else if (obj.closestPoint.splittingAxis != currentNode.splittingAxis) {
+      //* Do a search on the next axis
+      if (
+        target[splittingAxis] >= currentNode.point[splittingAxis] &&
+        currentNode.right != null
+      ) {
+        obj = this.NearestToTarget(target, currentNode.right, depth + 1);
+      } else if (currentNode.left != null) {
+        obj = this.NearestToTarget(target, currentNode.left, depth + 1);
+      }
     }
 
     return obj;
@@ -135,46 +109,52 @@ class KDTree {
     return Math.sqrt(cumulative);
   }
 }
-
-let limgrave = [
-  [73.8, 347.3],
-  [340.9, 603.8],
-  [90.5, 123.2],
-  [63.36, -88.02],
-  [36.4, -69.14],
-  [54.27, -26.14],
-  [104.47, -114.78],
-  [0, 44.8],
-  [-8.8, -18.51],
-  [98.48, 112.97],
-];
-
-let Calied = [
-  [151.76, 30.32],
-  [122.07, -82.49],
-  [162.27, -23.74],
-  [38.83, -82.52],
-  [145.6, 117.79],
-  [345.36, 14.79],
-  [-4.94, -111.65],
-  [91.76, 18.11],
-  [33.64, -43.98],
-];
-
-let Liurinia = [
-  [337.97, -71.04],
-  [151.21, -63.07],
-  [235.98, 51.3],
-  [261.09, -65.16],
-  [476.0, 11.35],
-  [367, -62.62],
-  [424, 120],
-  [288.41, 82.06],
-];
-
-const points = [...Calied, ...Liurinia, ...limgrave];
+const mapData = {
+  Limgrave: [
+    [73.8, 347.3],
+    [340.9, 603.8],
+    [90.5, 123.2],
+    [63.36, -88.02],
+    [36.4, -69.14],
+    [54.27, -26.14],
+    [104.47, -114.78],
+    [0, 44.8],
+    [-8.8, -18.51],
+    [98.48, 112.97],
+  ],
+  Calied: [
+    [151.76, 30.32],
+    [122.07, -82.49],
+    [162.27, -23.74],
+    [38.83, -82.52],
+    [145.6, 117.79],
+    [345.36, 14.79],
+    [-4.94, -111.65],
+    [91.76, 18.11],
+    [33.64, -43.98],
+  ],
+  Liurinia: [
+    [337.97, -71.04],
+    [151.21, -63.07],
+    [235.98, 51.3],
+    [261.09, -65.16],
+    [476.0, 11.35],
+    [367, -62.62],
+    [424, 120],
+    [288.41, 82.06],
+  ],
+};
+const points = Object.values(mapData).flat();
 const tree = new KDTree(null, points);
 tree.PrintRoot();
-console.log("The target point is: " + [122, -85])
-let a = tree.NearestToTarget([122, -85], tree.root);
+console.log("The target point is: " + [122, -85]);
+let a = tree.NearestToTarget([300, 129], tree.root);
+console.log(a);
 
+for (let key in mapData) {
+  mapData[key].some((el) => {
+    return el == a.closestPoint.point;
+  })
+    ? console.log(`Player is located in ${key}`)
+    : null;
+}
